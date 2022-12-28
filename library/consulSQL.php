7bf9d60eef73946213bd767b6d04c82b926475f1
Veronica Placencia <66141973+VeronicaPlacencia@users.noreply.github.com>
2022-12-28 17:46:59 -0500

    Update consulSQL.php

--- library/consulSQL.php
+++ library/consulSQL.php
@@ -2,10 +2,10 @@
 /* Clase para ejecutar las consultas a la Base de Datos*/
 class ejecutarSQL {
     public static function conectar(){
-        if(!$con=  mysqli_connect("db4free.net","pink2511","pink2511T","test")){
+        if(!$con=  mysqli_connect("localhost","root","","test")){
             die("Error en el servidor, verifique sus datos");
         }
-        if (!mysqli_select_db($con, "store_25")) {
+        if (!mysqli_select_db($con, "store")) {
             die("Error al conectar con la base de datos, verifique el nombre de la base de datos");
         }
         /* Codificar la información de la base de datos a UTF8*/
